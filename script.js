094f40e6df78e28e105d95d5e30d2990a0a20785
halsschmerzen <bastiansteampl@gmail.com>
2023-09-01 15:26:14 +0200

    Just some formatting in the playRound() function

--- script.js
+++ script.js
@@ -1,40 +1,52 @@
 function getComputerChoice() {
   let choice = Math.floor(Math.random() * 3);
-  
-//   I know this is a weird representation but:
-//   RANDOM NUMBERS STAND FOR:
-  
-//   ROCK = 0
-//   SCISSORS = 1
-//   PAPER = 2
+
+  //   I know this is a weird representation but:
+  //   RANDOM NUMBERS STAND FOR:
+
+  //   ROCK = 0
+  //   SCISSORS = 1
+  //   PAPER = 2
 
   return choice;
 }
 
 function playRound(playerSelection, computerSelection) {
   playerSelection = playerSelection.toLowerCase();
-  
+
   switch (playerSelection) {
     case "rock":
-        return computerSelection === 1 ? "You win! Rock beats Scissors!" :
-               computerSelection === 2 ? "You lose! Rock loses against Paper!" :
-               "Tie :§ - Rock vs Rock";
+      return computerSelection === 1
+        ? "You win! Rock beats Scissors!"
+        : computerSelection === 2
+        ? "You lose! Rock loses against Paper!"
+        : "Tie :§ - Rock vs Rock";
     case "paper":
-        return computerSelection === 0 ? "You win! Paper beats Rock!" : 
-               computerSelection === 2 ? "You lose! Paper loses against Scissors!" :
-               "Tie :( - Paper vs Paper"
+      return computerSelection === 0
+        ? "You win! Paper beats Rock!"
+        : computerSelection === 2
+        ? "You lose! Paper loses against Scissors!"
+        : "Tie :( - Paper vs Paper";
     case "scissors":
-        return computerSelection === 2 ? "You win! Scissors beats Paper!" :
-               computerSelection === 0 ? "You lose! Scissors loses against Rock!" :
-               "Tie D: - Scissors vs Scissors"     
+      return computerSelection === 2
+        ? "You win! Scissors beats Paper!"
+        : computerSelection === 0
+        ? "You lose! Scissors loses against Rock!"
+        : "Tie D: - Scissors vs Scissors";
     default:
-        return "Invalid input."
-  } 
+      return "Invalid input.";
+  }
 }
 
-const computerSelection = getComputerChoice();
+// const computerSelection = getComputerChoice();
 
-const playerSelection = prompt("Enter your choice: Rock - Paper - Scissors");
+// const playerSelection = prompt("Enter your choice: Rock - Paper - Scissors");
 
+function game() {
+  const computerSelection = getComputerChoice();
+  const playerSelection = prompt("Enter your choice: Rock - Paper - Scissors");
+  console.log(playRound(playerSelection, computerSelection));
+}
 
-console.log(playRound(playerSelection, computerSelection))
+game()
+
