de24f0180325be6aa2b470f552f638118d1e43cf
halsschmerzen <bastiansteampl@gmail.com>
2023-09-04 13:39:41 +0200

    Working game that - Score Indicator needs work

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+// Generate variables that are updated after the game logic decided who won/lost
+let playerScore = 0;
+let computerScore = 0;
+// DOM Selection for Web-Application
+const container = document.querySelector("#container");
+// Generate a text for each the AI, the Player and the current score
+const resultText = document.createElement("div");
+
+resultText.classList.add("resultText");
+container.appendChild(resultText);
+
 function getComputerChoice() {
   let choice = Math.floor(Math.random() * 3);
 
@@ -11,40 +22,64 @@ function getComputerChoice() {
   return choice;
 }
 
+
+
 function playRound(playerSelection, computerSelection) {
   playerSelection = playerSelection.toLowerCase();
   switch (playerSelection) {
     case "rock":
       return computerSelection === 1
-        ? "You win! Rock beats Scissors!"
+        ? playerScore++
         : computerSelection === 2
-        ? "You lose! Rock loses against Paper!"
-        : "Tie :§ - Rock vs Rock";
+        ? computerScore++
+        : "Tie!";
     case "paper":
       return computerSelection === 0
-        ? "You win! Paper beats Rock!"
+        ? playerScore++
         : computerSelection === 2
-        ? "You lose! Paper loses against Scissors!"
-        : "Tie :( - Paper vs Paper";
+        ? computerScore++
+        : "Tie!";
     case "scissors":
       return computerSelection === 2
-        ? "You win! Scissors beats Paper!"
+        ? playerScore++
         : computerSelection === 0
-        ? "You lose! Scissors loses against Rock!"
-        : "Tie D: - Scissors vs Scissors";
+        ? computerScore++
+        : "Tie!";
     default:
       return "Invalid input.";
   }
 }
 
-// const computerSelection = getComputerChoice();
+function updateScore() {
 
-// const playerSelection = prompt("Enter your choice: Rock - Paper - Scissors");
-
-function game() {
-//  const computerSelection = getComputerChoice();
-    // Removed initialization of the game for preparation of Web-Based Application
 }
 
-game()
+function checkGameEnd() {
+  if(playerScore >= 5 || computerScore >= 5) {
+    if(playerScore > computerScore) {
+      resultText.textContent = "You won against the AI 5 times! You are the best!"
+    } else if(computerChoice > playerScore) {
+      resultText.textContent = "AI is conspiring against us! You lost!"
+    } else {
+      resultText.textContent = "Guess it's a tie. The world is even now. Ying and Yang."
+    }
+  }
+}
 
+
+const mainButtons = container.querySelectorAll("button");
+
+mainButtons.forEach((button) => {
+  button.addEventListener("click", (event) => {
+    const clickedButton = event.target;
+    const buttonText = clickedButton.textContent.toLowerCase();
+    const computerChoice = getComputerChoice();
+    const roundResult = playRound(buttonText, computerChoice);
+    // I want to check what button was clicked simply by checking the given HTML Text
+    // Content, and if it matches the clicked Button.
+    updateScore();
+    resultText.textContent = roundResult;
+
+    checkGameEnd();
+  });
+});
